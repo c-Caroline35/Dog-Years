c3be7a7291e706a9723e519f4a42a01cc0ad204a
c-Caroline35 <104658845+c-Caroline35@users.noreply.github.com>
2022-04-29 19:16:02 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+var myAge = '23';
+// I used var to point to my age and say that is equaled to the amount of 23 because that is my current age.  
+
+var earlyYears ='2'; 
+// I used var to point to the number 2 and call it early years. 
+
+earlyYears *= 10.5;
+
+let laterYears = myAge -2;
+// I Wanted to use let to simplify the code and ensure it is calculated correctly.
+
+laterYears *= 4;
+// The *= part of the code allows for a simple and immediate answer.
+
+console.log(earlyYears);
+console.log(laterYears);
+//Above is me checking my work 
+
+let myAgeInDogYears = earlyYears += laterYears; 
+
+let myName = 'Caroline'.toLowerCase();
+// Above is going to make my full name all lower case. This is a simple way to do that. 
+
+console.log(`My name is ${myName} I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`); 
+//The results in sentence form. 
